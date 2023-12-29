81583ee3be1a8f88feb72cf588749f8d5fd62386
Jonas Huebner <dev@jonashuebner.de>
2023-12-29 16:05:02 +0100

    feat: implement graphviz diagrams

--- src/graphix/draw.py
+++ src/graphix/draw.py
@@ -1,5 +1,6 @@
 from src.model.modules import ModuleGraph
+from .draw_with_graphviz import draw
 
 
-def draw_dependency_graph(model: ModuleGraph):
-    print(model.modules)
+def draw_dependency_graph(module_graph: ModuleGraph):
+    draw(module_graph)
