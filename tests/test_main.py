e100b95c97ffb2ee868525e2f995b9c65f34a45e
Jonas Huebner <dev@jonashuebner.de>
2024-01-17 14:33:33 +0100

    feat: pass default via cli arguments

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 import unittest
+from assertpy import assert_that
 
 from src import main
 
@@ -6,7 +7,17 @@ from src import main
 class MyTestCase(unittest.TestCase):
 
     def test_does_not_throw_any_exception(self):
-        main.main()
+        main.main(".dependency-graphics-rc.yaml")
+
+    def test_does_consider_custom_config(self):
+        args = main.parse_args(["-c", ".i_am_not_a_default.yaml"])
+
+        assert_that(args.config).is_equal_to(".i_am_not_a_default.yaml")
+
+    def test_use_default(self):
+        args = main.parse_args([])
+
+        assert_that(args.config).is_equal_to(".dependency-graphics-rc.yaml")
 
 
 if __name__ == '__main__':
