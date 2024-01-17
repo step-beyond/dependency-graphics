24b93fd9bac06dc8b7c5ed001089f15010527a5b
Jonas Huebner <dev@jonashuebner.de>
2024-01-17 14:33:33 +0100

    feat: pass default via cli arguments

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import argparse
+import sys
+
 from src.configuration import factory as config_factory
 from src.configuration.config import Config
 from src.transformation import transformation
@@ -5,11 +8,19 @@ from src.graphix import draw
 from src.parser import parse
 
 
-def main():
-    config: Config = config_factory.load_config("./.dependency-graphics-rc.yaml")
+def main(config_file_path: str):
+    config: Config = config_factory.load_config(config_file_path)
     module_tree = parse.parse_module_tree(config)
     module_tree = transformation.transform_model(module_tree, config)
     draw.draw_dependency_graph(module_tree, config)
 
 
-main()
+def parse_args(plain_args: []):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', default=".dependency-graphics-rc.yaml")
+    parsed_args, _ = parser.parse_known_args(plain_args)
+    return parsed_args
+
+
+args = parse_args(sys.argv[1:])
+main(args.config)
